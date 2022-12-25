fcea882a410a69c674bff360fb021c7890738481
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2022-12-25 11:43:07 +1100

    Add INSTANCE_NAME config value, fix branding name patch

--- DiscordClientProxy/ClientPatches/Branding/BrandingNamePatch.cs
+++ DiscordClientProxy/ClientPatches/Branding/BrandingNamePatch.cs
@@ -9,14 +9,14 @@ public class BrandingNamePatch : ClientPatch
     {
         if (!content.Contains("Discord")) return content;
         Console.WriteLine($"[ClientPatch:{GetType().Name}] Applying patch...");
-        content = content.Replace(" Discord ", " ${INSTANCE_NAME} ");
-        content = content.Replace("Discord ", "${INSTANCE_NAME} ");
-        content = content.Replace(" Discord", " ${INSTANCE_NAME}");
-        content = content.Replace("Discord Premium", "${INSTANCE_NAME} Premium");
-        content = content.Replace("Discord Nitro", "${INSTANCE_NAME} Premium");
-        content = content.Replace("Discord's", "${INSTANCE_NAME}'s");
+        content = content.Replace(" Discord ", $" {Configuration.Instance.InstanceName} ");
+        content = content.Replace("Discord ", $"{Configuration.Instance.InstanceName} ");
+        content = content.Replace(" Discord", $" {Configuration.Instance.InstanceName}");
+        content = content.Replace("Discord Premium", $"{Configuration.Instance.InstanceName} Premium");
+        content = content.Replace("Discord Nitro", $"{Configuration.Instance.InstanceName} Premium");
+        content = content.Replace("Discord's", $"{Configuration.Instance.InstanceName}'s");
         //content = content.Replace("DiscordTag", "FosscordTag");
-        content = content.Replace("*Discord*", "*${INSTANCE_NAME}*");
+        content = content.Replace("*Discord*", $"*{Configuration.Instance.InstanceName}*");
         return content;
     }
 }
